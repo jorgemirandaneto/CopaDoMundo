7b6771fe77a94e3f7222a1ca935c1dbe43548804
Jorge Miranda <jorge.miranda@sam.fortaleza.ce.gov.br>
2017-06-21 15:47:06 -0300

    atualizão na parte de login

---
+++ CopaDoMundo/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using CopaDoMundo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CopaDoMundo.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // POST : Usuario
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model,string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/"))
+                    {
+                        return Redirect(returnUrl); 
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index");       
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("","Usuario e senha está incorreto.");
+                }
+            }return View(model);
+        }
+    }
+}
